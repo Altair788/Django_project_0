46b8831cdf842baafe06a2b94a1a4bb8d84b56a2
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-26 20:07:27 +0300

    20_2 шаблонизация, вывели студентов на главную страницу

--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from main.models import Student
+
 
 # Create your views here.
+# def index(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         email = request.POST.get("email")
+#         message = request.POST.get("message")
+#         print(f"name: {name}\nemail: {email}\nmessage: {message}\n")
+#     return render(request, 'main/index.html')
+
+
 def index(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
-        print(f"name: {name}\nemail: {email}\nmessage: {message}\n")
-    return render(request, 'main/index.html')
+    students_list = Student.objects.all()
+    context = {
+        'object_list': students_list
+    }
+    return render(request, 'main/index.html', context)
