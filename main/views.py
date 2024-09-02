f10bf570fdddd2956e3c7388cfe0b31b12438dfa
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-09-02 17:31:13 +0300

    20_2

--- main/views.py
+++ main/views.py
@@ -3,19 +3,23 @@ from django.shortcuts import render
 from main.models import Student
 
 
-# Create your views here.
-# def index(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         email = request.POST.get("email")
-#         message = request.POST.get("message")
-#         print(f"name: {name}\nemail: {email}\nmessage: {message}\n")
-#     return render(request, 'main/index.html')
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        print(f"name: {name}\nemail: {email}\nmessage: {message}\n")
+
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'main/contact.html', context)
 
 
 def index(request):
     students_list = Student.objects.all()
     context = {
-        'object_list': students_list
+        'object_list': students_list,
+        'title': 'Главная'
     }
     return render(request, 'main/index.html', context)
