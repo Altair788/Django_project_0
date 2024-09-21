00045a33dde23a3c36cec8c699766772db619826
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-09-21 20:52:40 +0300

    21_1 FBV -> CBV

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from main.models import Student
 
@@ -16,10 +17,28 @@ def contact(request):
     return render(request, 'main/contact.html', context)
 
 
-def index(request):
-    students_list = Student.objects.all()
-    context = {
-        'object_list': students_list,
-        'title': 'Главная'
-    }
-    return render(request, 'main/index.html', context)
+
+class StudentListView(ListView):
+    model = Student
+    template_name = 'main/index.html'
+#
+# def index(request):
+#     students_list = Student.objects.all()
+#     context = {
+#         'object_list': students_list,
+#         'title': 'Главная'
+#     }
+#     return render(request, 'main/index.html', context)
+
+
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'main/student_detail.html'
+# def view_student(request, pk):
+#     student_item = get_object_or_404(Student, pk=pk)
+#     context = {
+#         'object': student_item
+#     }
+#     return render(request, 'main/student_detail.html', context)
