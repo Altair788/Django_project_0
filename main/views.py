99d8a659fd5d310d64e4569f8e2d5b451eea72f1
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-09-27 18:01:38 +0300

    пока ничего не понятно и не работает сайт

--- main/views.py
+++ main/views.py
@@ -22,22 +22,8 @@ class StudentListView(ListView):
     model = Student
 
 
-def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
-
-    context = {
-        'title': 'Контакты'
-    }
-    return render(request, 'main/contact.html', context)
-
-
 class StudentDetailView(DetailView):
     model = Student
-
 
 
 class StudentCreateView(CreateView):
