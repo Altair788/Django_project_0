f197a187e4f572a40228734661775747d803644a
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-10-12 19:52:27 +0300

    реализована верификация по ссылке в почте и токену

--- users_alt/urls.py
+++ users_alt/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users_alt.apps import UsersAltConfig
-from users_alt.views import UserCreateView
+from users_alt.views import UserCreateView, email_verification
 
 app_name = UsersAltConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users_alt/user_login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(template_name='users_alt/user_form.html'), name='register'),
+    path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
 ]
