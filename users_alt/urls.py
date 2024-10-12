118f8de9ab6da26a4d8100baaf0b2fb10621d070
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-10-12 17:20:59 +0300

    добавил регистрацию пользователя

--- users_alt/urls.py
+++ users_alt/urls.py
@@ -2,11 +2,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users_alt.apps import UsersAltConfig
-
+from users_alt.views import UserCreateView
 
 app_name = UsersAltConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users_alt/user_login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='users_alt/user_logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', UserCreateView.as_view(template_name='users_alt/user_form.html'), name='register'),
 ]
--- users_alt/views.py
+++ users_alt/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users_alt.forms import UserRegisterForm
+from users_alt.models import User
+
+
+class UserCreateView(CreateView):
+
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users_alt:login")
